4d04f6929f708246bcdf4305f3276beb55ec7908
MUHAMMAD ALI MUGHAL <itsmuhammadali125@gmail.com>
2024-07-26 00:36:37 +0500

    Delete chapter 1 .ps directory

--- chapter 1 .ps/problem  4.py
+++
@@ -1,4 +0,0 @@
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("My name is muhammad ali im am 19 year old")
-engine.runAndWait()
--- chapter 1 .ps/problem 2.py
+++
@@ -1,2 +0,0 @@
-print("""using repel to ptint 5 table """)
-# window powershell per ja kar phr python likhna ha or phr 5 ka table likhna ha 
